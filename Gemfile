bc70f913c300badb4909e49df6cb27869007083f
jennifer-yoo <jennifer.yoo7@gmail.com>
2020-08-06 20:41:11 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+
 gem 'rails', '5.0.7.1'
 gem 'sqlite3', '~>1.3.6'
 gem 'sass-rails', '~> 5.0'
@@ -7,6 +8,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootsnap'
+
 
 group :development, :test do
   gem 'pry'
@@ -16,3 +18,4 @@ end
 
 group :development do
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a1 = Artist.create(name: "Adele")
+a2 = Artist.create(name: "Beyonnce")
+a3 = Artist.create(name: "Kanye")
+
+Song.create(title: "Hello", artist_id: a1.id )
+Song.create(title: "Halo", artist_id: a2.id )
+Song.create(title: "Single Ladies", artist_id: a2.id )
+Song.create(title: "Listen", artist_id: a2.id )
+Song.create(title: "Good Life", artist_id: a3.id )
+
